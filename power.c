c38de010dabad357fa6bac86a1c81efed3c50932
duraimuruganmech <35653042+duraimuruganmech@users.noreply.github.com>
2018-01-28 09:37:33 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    printf("enter value");
+    scanf("%d",&a);
+    printf("enter values");
+    scanf("%d",&b);
+    c=pow(a,b);
+    printf("%d",c);
+}
