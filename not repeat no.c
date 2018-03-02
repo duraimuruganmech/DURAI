19099fcce37c59a8e3e9368686d375986b81bb02
duraimuruganmech <35653042+duraimuruganmech@users.noreply.github.com>
2018-03-02 15:00:39 +0530

    Create not repeat no.c

---
+++ not repeat no.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main() 
+{
+	int a[10],n,i,j,count=0,ans;
+	printf("Enter no  of  elements");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	scanf("%d",&a[i]);
+	for(i=1;i<=n;i++)
+	for(j=i+1;j<=n;j++)
+	{
+	if(a[i]!=a[j])
+	{
+	++count;
+	if(count>0)
+	ans=a[i];	
+	}
+	}
+	printf("%d",ans);	
+}
