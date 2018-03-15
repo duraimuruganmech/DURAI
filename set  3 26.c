fda5548f5019efa963ff2205053f5be17e25cb1b
duraimuruganmech <35653042+duraimuruganmech@users.noreply.github.com>
2018-03-15 16:07:05 +0530

    Create set  3 26.c

---
+++ set  3 26.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n,a[10],s,m;
+printf("Enter limit");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=1;j<=n;j++)
+{
+for(m=j+1;m<=n;m++)
+{
+if(a[j]>=a[m])
+{
+s=a[j];
+a[j]=a[m];
+a[m]=s;
+}
+}
+}
+printf("%d",s);
+}
